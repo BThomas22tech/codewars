587f11a947e01639f621e244ec125d52d7bc26fd
Brandon Thomas <bran_thom@yahoo.com>
2023-04-05 23:28:59 -0500

    more exercises

--- code_wars.py
+++ code_wars.py
@@ -36,3 +36,32 @@ print(make_negative(4))
 print(make_negative(-3))
 print(make_negative(-12))
 print(make_negative(1))
+
+def find_average(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    return sum / len(numbers)
+print(find_average([1, 2, 3]))
+
+def bmi(weight, height):
+    bmi = weight / (height * height)
+    if bmi <= 18.5:
+        return "Underweight"
+    if bmi <= 25.0:
+        return "Normal"
+    if bmi <= 30.0:
+        return "Overweight"
+    if bmi > 30:
+        return "Obese"
+print(bmi(50, 1.80))
+print(bmi(80, 1.80))
+print(bmi(110, 1.80))
+
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+def grow(arr):
+    mult = 1
+    for i in arr:
+        mult *= i
+    return mult
