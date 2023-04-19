90282e8fab7744e888b5513c39483c0ac3e6eba2
Brandon Thomas <bran_thom@yahoo.com>
2023-04-19 13:37:13 -0500

    new algos

--- code_wars.py
+++ code_wars.py
@@ -127,3 +127,18 @@ def better_than_average(class_points, your_points):
         return True
     else:
         return False
+
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+print(greet("John"))
+
+
+def square_sum(numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i*i
+    return sum 
