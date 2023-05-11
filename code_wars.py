400b4051d42f94743eedffa444992434e6652b80
BThomas <bran_thom@yahoo.com>
2023-05-11 12:44:15 -0500

    two pointers linked list

--- code_wars.py
+++ code_wars.py
@@ -221,3 +221,36 @@ def get_age(age):
     number = int(age[0])
     print(number)
     return number
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        while headA != headB:
+            if headA == None:
+                headA = headB
+            else: 
+                headA = headA.next
+            if headB == None:
+                headB = headA
+            else: 
+                headB = headB.next
+
+        # return headA
+    
+        a=headA
+        b=headB
+        while a!=b:
+            if a==None:
+                a=headB
+            else:
+                a=a.next
+            if b==None:
+                b=headA
+            else:
+                b=b.next
+        return a
