b98aa925f54e2579a2af1f6bbefcfa10deaf694e
BThomas <bran_thom@yahoo.com>
2023-05-22 14:50:54 -0500

    unique string with dictionary

--- code_wars.py
+++ code_wars.py
@@ -254,3 +254,44 @@ class Solution:
             else:
                 b=b.next
         return a
+
+
+def unique(string):
+    dict = {"a":0,
+            "b":0,
+            "c":0,
+            "d":0,
+            "e":0,
+            "f":0,
+            "g":0,
+            "h":0, 
+            "i":0, 
+            "j":0,
+            "k":0,
+            "l":0,
+            "m":0,
+            "n":0,
+            "o":0,
+            "p":0,
+            "q": 0,
+            "r":0,
+            "s":0,
+            "t":0,
+            "u":0,
+            "v":0,
+            "w":0,
+            "x":0,
+            "y":0,
+            "z":0}
+    for i in string:
+        if i in dict:
+            dict[i] += 1
+    for k in dict:
+        if dict[k] <= 1:
+            print(k, "is unique")
+        else: print(k, "is not unique")
+    return dict
+
+print(unique("high"))
+print(unique("cool"))
+print(unique("egg"))
